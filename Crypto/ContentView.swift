104d55ba5a5dc198f53041057042c39bbbfc82be
nafe333 <94410012+nafe333@users.noreply.github.com>
2026-03-21 03:08:17 +0200

    Initial Commit

---
+++ Crypto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Crypto
+//
+//  Created by Nafea Elkassas on 21/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Nafea Elkassas on 21/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
