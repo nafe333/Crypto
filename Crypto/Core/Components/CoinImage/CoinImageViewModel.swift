4b9287a3e3028804157621d903c0add32b101f62
nafe333 <94410012+nafe333@users.noreply.github.com>
2026-03-29 03:45:18 +0200

    added coin image view, serviceand view model

---
+++ Crypto/Core/Components/CoinImage/CoinImageViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CoinImageViewModel.swift
+//  Crypto
+//
+//  Created by Nafea Elkassas on 29/03/2026.
+//
+
+import Foundation
---
+++ Crypto/Services/CoinImageService.swift
@@ -0,0 +1,8 @@
+//
+//  CoinImageService.swift
+//  Crypto
+//
+//  Created by Nafea Elkassas on 29/03/2026.
+//
+
+import Foundation
