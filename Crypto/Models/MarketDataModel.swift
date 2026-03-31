bf024873684b745fe8d055fc4e40f4de29d77e7d
nafe333 <94410012+nafe333@users.noreply.github.com>
2026-03-31 02:18:52 +0200

    added the market data model and linked with home view model via marketData service

---
+++ Crypto/Models/MarketDataModel.swift
@@ -0,0 +1,8 @@
+//
+//  MarketDataModel.swift
+//  Crypto
+//
+//  Created by Nafea Elkassas on 31/03/2026.
+//
+
+import Foundation
---
+++ Crypto/Services/MarketDataService.swift
@@ -0,0 +1,8 @@
+//
+//  MarketDataService.swift
+//  Crypto
+//
+//  Created by Nafea Elkassas on 31/03/2026.
+//
+
+import Foundation
