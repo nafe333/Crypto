31e3285a003d51577523b628dbef4be01b255d03
nafe333 <94410012+nafe333@users.noreply.github.com>
2026-04-06 23:07:58 +0200

    added the chart view and replaced empty text in detail view with it

---
+++ Crypto/Core/DetailView/Views/ChartView.swift
@@ -0,0 +1,18 @@
+//
+//  ChartView.swift
+//  Crypto
+//
+//  Created by Nafea Elkassas on 06/04/2026.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChartView()
+}
---
+++ Crypto/Extensions/Date.swift
@@ -0,0 +1,8 @@
+//
+//  Data.swift
+//  Crypto
+//
+//  Created by Nafea Elkassas on 06/04/2026.
+//
+
+import Foundation
