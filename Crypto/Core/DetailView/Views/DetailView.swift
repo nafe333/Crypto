3c6cf1560a66381582df6833c4587c7e097ebe3b
nafe333 <94410012+nafe333@users.noreply.github.com>
2026-04-06 03:36:00 +0200

    added new details view and loading detail view and navigated there using navigation link

---
+++ Crypto/Core/DetailView/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Crypto
+//
+//  Created by Nafea Elkassas on 05/04/2026.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
