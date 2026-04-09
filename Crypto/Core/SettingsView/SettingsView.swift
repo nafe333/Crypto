89b9c37f0db51950bca4eae2630847c35a3b34a2
nafe333 <94410012+nafe333@users.noreply.github.com>
2026-04-09 18:23:46 +0200

    added settings view and linked with home view

---
+++ Crypto/Core/SettingsView/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Crypto
+//
+//  Created by Nafea Elkassas on 09/04/2026.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
