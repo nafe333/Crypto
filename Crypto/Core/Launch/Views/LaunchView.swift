21de5ccc6c8564df0dbbd7f135dd8418abc7afbf
nafe333 <94410012+nafe333@users.noreply.github.com>
2026-04-12 22:19:20 +0200

    added lauch screen animation then navigate to home view

---
+++ Crypto/Core/Launch/Views/LaunchView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchView.swift
+//  Crypto
+//
+//  Created by Nafea Elkassas on 12/04/2026.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchView()
+}
